5ce439c2cc4e3dc1c5bd497b9fb8160f66bd770c
Chaos Shen <chaosdefinition@hotmail.com>
2016-03-21 11:37:32 +0800

    Hello world!

---
+++ README.md
@@ -0,0 +1,3 @@
+# hyperspark
+
+hyperspark is a distributed computing system used to compute the circular wirelength of hypercubes of specified dimension.
---
+++ src/main/java/me/chaosdefinition/hyperspark/App.java
@@ -0,0 +1,11 @@
+package me.chaosdefinition.hyperspark;
+
+/**
+ * @author Chaos Shen
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
